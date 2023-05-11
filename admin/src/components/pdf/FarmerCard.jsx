b320e65f7fc2947c67f55a6bcb457e0e9a68f9d2
Md. Harun Or Rashid <horrashid007@gmail.com>
2023-05-12 02:56:51 +0600

    fixed bangla font issue

--- admin/src/components/pdf/FarmerCard.jsx
+++ admin/src/components/pdf/FarmerCard.jsx
@@ -5,7 +5,7 @@ import { useGetFarmerCardQuery } from '@/store/apiSlices/farmerCardApiSlice';
 import { fDate } from '@/utils/formatTime';
 
 Font.register({
-  family: 'SutonnyMJ',
+  family: 'SolaimanLipi',
   fonts: [{ src: '/fonts/SolaimanLipi.ttf' }],
 });
 
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
-    fontFamily: 'SutonnyMJ',
+    fontFamily: 'SolaimanLipi',
     backgroundColor: '#fff',
     padding: 30,
   },
@@ -126,9 +126,9 @@ function FarmerCard() {
             <View style={styles.container}>
               <Image style={styles.image} src={farmer.image} />
               <View style={styles.infoWrapper}>
-                <Text style={styles.text}>Name: {farmer.name}</Text>
+                <Text style={styles.text}>Name: {farmer.name + ' '}</Text>
                 <Text style={styles.text}>Phone: {farmer.phone}</Text>
-                <Text style={styles.text}>Address: {farmer.address}</Text>
+                <Text style={styles.text}>Address: {farmer.address + ' '}</Text>
                 <Text style={styles.text}>Card Number: {cardNumber}</Text>
                 <Text style={styles.text}>ID NO: {farmer.identity}</Text>
               </View>
@@ -136,7 +136,7 @@ function FarmerCard() {
             <View style={styles.footer}>
               <View>
                 <Text style={styles.heading}>Issued By</Text>
-                <Text style={styles.footerText}>Name: {createdBy.name}</Text>
+                <Text style={styles.footerText}>Name: {createdBy.name + ' '}</Text>
                 <Text style={styles.footerText}>Designation: {createdBy.role}</Text>
                 <Text style={styles.footerText}>Phone: {createdBy.phone}</Text>
                 <Text style={styles.footerText}>Date: {fDate(createdAt)}</Text>
